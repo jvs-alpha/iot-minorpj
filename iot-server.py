c31fcf3008211ebf20b9a370cbfcbd315498315d
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-07-23 01:32:12 +0530

    made some changes to io-server.py

--- iot-server.py
+++ iot-server.py
@@ -1,18 +1,19 @@
 from flask import Flask,request
 from flask_restful import Api,Resource
-import csv
+import pandas as pd
 
 app = Flask(__name__)
 api = Api(app)
 
-class base(Resource):
+class store(Resource):
     def post(self):
         dat = request.get_json()
         dict_dat = dict(dat)
-        print(dict_dat)
+        dataframe = pd.DataFrame(dict_dat)
+        dataframe.to_csv("data.csv")
         return dict_dat,200
 
-api.add_resource(base,"/")
+api.add_resource(store,"/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="5000",debug=True)
