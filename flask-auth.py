716dd4d165b92e57b4399ebc318f26d4d90934e7
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-08-15 00:42:03 +0530

    added some changes to flask-auth.py

--- flask-auth.py
+++ flask-auth.py
@@ -3,18 +3,32 @@ from flask_restful import Api,Resource
 import jwt
 import datetime
 
+token = ""
+
 app = Flask(__name__)
 api = Api(app)
 
 class auth(Resource):
     def get(self):
+        global token
         if request.authorization and request.authorization.username == "jvs" and request.authorization.password == "ggsafehouse":
             encoded = jwt.encode({"user":request.authorization.username,"exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=30)},"thisisshit",algorithm="HS256")
-            return encoded.decode("UTF-8"),200
+            token = encoded.decode("UTF-8")
+            return token,200
         else:
             return make_response("you are dead",200)
 
+class protected(Resource):
+    def get(self):
+        global token
+        if request.headers["key"] ==  token:
+            return "GGWP",200
+        else:
+            return "YOU ARE DEAD",401
+
+
 api.add_resource(auth,"/login")
+api.add_resource(protected,"/protected")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="3000",debug=True)
