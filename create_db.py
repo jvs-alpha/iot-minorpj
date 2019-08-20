c243eb81d60a1a4f8d619df8fd5387ece3372090
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-08-20 10:31:29 +0530

    added some changes to the create_db.py

--- create_db.py
+++ create_db.py
@@ -8,6 +8,12 @@ db = SQLAlchemy(app)
 
 class node(db.Model):
     id = db.Column(db.Integer,primary_key=True)
+    pub_id = db.Column(db.String(40),unique=True,nullable=False)
     nodename = db.Column(db.String(20),unique=True,nullable=False)
     encode_jwt = db.Column(db.String(200),unique=True,nullable=False)
     updated_time = db.Column(db.DateTime,nullable=True,default=datetime.datetime.utcnow())
+
+# for creating the databse import db from this file in python3 and run
+# db.create_all() for creating the database
+# then we need to import the node class to put the data in the as varialbe
+# then add and commit the data to the database
