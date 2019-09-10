aa5b1894f1b7a4ba42fab7923c468bc0c53d7d49
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-09-10 13:45:03 +0530

    added sqlite to the flask4.py

--- read_db.py
+++ read_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-database = sqlite3.connect("data.db")
+database = sqlite3.connect("dat.db")
 cursor = database.cursor()
-data = cursor.execute("SELECT * FROM DATA")
+data = cursor.execute("SELECT * FROM node")
 print(list(data))
 database.close()
