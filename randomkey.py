7db5ead8a55400ffe28252df96f603207b68b2e4
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-09-21 23:29:14 +0530

    removed iot-server and randomkey

--- randomkey.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def key_gen():
-    key = "".join(random.choices(string.ascii_letters+string.digits,k=100))
-    return key
-
-if __name__ == "__main__":
-    print(key_gen())
