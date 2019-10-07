39a42a77202a421588b23efd8c6912b8ea473249
jvs-alpha <jvsamuel.rufus@gmail.com>
2019-10-07 23:58:58 +0530

    added some changes to the flask-mark4

--- flask-mark4.py
+++ flask-mark4.py
@@ -1,10 +1,11 @@
+'''
+we need to have two sections in the iot node one to get the authentication jwt from the dat2.db
+and the second one is for using it to control the switch
+'''
+
 #!/usr/bin/python3
 from flask import Flask,request
-from flask_restful import Resource,Api
 from read_node_db import *
 from controls import *
 import socket
 import sqlite3
-
-app = Flask(__name__)
-api = Api(app)
